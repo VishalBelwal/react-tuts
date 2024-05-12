4b92c95a737364a06a80eb1618efade11b9314e1
vishalBelwal <vishalbelwal52@gmail.com>
2024-05-13 01:28:41 +0530

    react

--- 02_counter_project/src/App.jsx
+++ 02_counter_project/src/App.jsx
@@ -1,33 +1,42 @@
+// ui updation ko react control karta hai
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [counter, setCounter] = useState(0)        //yaha basically ham hooks use kar rahe hai  
+  //setCounter yani dusra function dusri value leta hai ki ye batao ki ab iske andar konsi value dale
+
+  // let counter = 5
+
+  
+  const addValue = () => {
+    // const counter = counter + 1
+    if (counter >= 20) {
+      alert("Value is greater than 20")
+    } else {
+      setCounter(counter + 1)   // value updation like this
+    }             
+  }
+
+  const removeValue = () => {
+    // let counter = counter - 1
+    if (counter > 0) {
+      setCounter(counter - 1)               // value updation like this
+    } else {
+      alert("Value is less than 0")
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Chai Aur React</h1>
+        <h2>Counter value: {counter}</h2>
+
+        <button onClick={addValue}>Add value</button>
+        <br />
+        <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
